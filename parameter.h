70e3b044157d11d7f1d1fec0a7d59f3c0c297ca7
lorenzorizzo01 <s323185@tudenti.polito.it>
2025-07-16 14:25:42 +0000

    update parameter 32x32

--- parameter.h
+++ parameter.h
@@ -8,9 +8,9 @@
 #define CONV_0_STRIDE 1 
 #define CONV_0_ICH_PAR 1
 
-#define CONV_0_ICH 2// input channels
-#define CONV_0_IW 7 // input width
-#define CONV_0_IH 7 // input height
+#define CONV_0_ICH 3// input channels
+#define CONV_0_IW 32 // input width
+#define CONV_0_IH 32 // input height
 #define CONV_0_INPUT_SIZE (CONV_0_ICH * CONV_0_IW * CONV_0_IH) // input size
 
 
@@ -23,7 +23,7 @@
     constexpr int WINDOW_IN = CONV_0_IW / CONV_0_FW;
 #endif
 
-#define CONV_0_OCH 2 // output channels
+#define CONV_0_OCH 8 // output channels
 #define CONV_0_OW ((CONV_0_IW - CONV_0_FW) / CONV_0_STRIDE + 1) // output width
 #define CONV_0_OH ((CONV_0_IH - CONV_0_FH) / CONV_0_STRIDE + 1) // output height
 constexpr int CONV_0_INPUT_SIZE_MAX =  CONV_0_FH * WINDOW_IN * CONV_0_FW *WINDOW_IN * CONV_0_ICH; // input size
@@ -33,10 +33,10 @@ constexpr int KERNEL_SIZE_0 = CONV_0_FW * CONV_0_FH * CONV_0_ICH * CONV_0_OCH;
 #define CONV_1_STRIDE 1
 #define CONV_1_ICH_PAR 1
 
-#define CONV_1_FW 3 // filter width 
-#define CONV_1_FH 3 // filter height
+#define CONV_1_FW 1 // filter width 
+#define CONV_1_FH 1 // filter height
 
-#define CONV_1_OCH 2 // Numero di canali output
+#define CONV_1_OCH 8 // Numero di canali output
 #define CONV_1_OW ((CONV_0_OW - CONV_1_FW) / CONV_1_STRIDE + 1) // Larghezza output
 #define CONV_1_OH ((CONV_0_OH - CONV_1_FH) / CONV_1_STRIDE + 1) // Altezza output
 
@@ -48,14 +48,14 @@ constexpr int KERNEL_SIZE_0 = CONV_0_FW * CONV_0_FH * CONV_0_ICH * CONV_0_OCH;
 
 #define CONV_0_OUTPUT_SIZE (CONV_0_FH * WINDOW_OUT * CONV_0_FW * WINDOW_OUT * CONV_0_OCH) // output size
 constexpr int KERNEL_SIZE_1 = CONV_1_FW * CONV_1_FH * CONV_0_OCH * CONV_1_OCH; // kernel size
-
+#define CONV_1_OUTPUT_SIZE (CONV_1_OH * CONV_1_OW * CONV_1_OCH) // output size
 //---------------------------CONV_2--------------------------------
 #define CONV_2_STRIDE 1
 #define CONV_2_ICH_PAR 1
 #define CONV_2_FW 1 // filter width 
 #define CONV_2_FH 1 // filter height
 
-#define CONV_2_OCH 2 // input channels
+#define CONV_2_OCH 8 // input channels
 #define CONV_2_OW ((CONV_1_OW - CONV_2_FW) / CONV_2_STRIDE + 1) // output width
 #define CONV_2_OH ((CONV_1_OH - CONV_2_FH) / CONV_2_STRIDE + 1) // output height
 
@@ -65,7 +65,7 @@ constexpr int KERNEL_SIZE_1 = CONV_1_FW * CONV_1_FH * CONV_0_OCH * CONV_1_OCH; /
     #define WINDOW_OUT_2 (CONV_1_OW / CONV_2_FW)
 #endif
 
-#define CONV_1_OUTPUT_SIZE (CONV_1_FH * WINDOW_OUT * CONV_1_FW * WINDOW_OUT * CONV_1_OCH) // output size
+//#define CONV_1_OUTPUT_SIZE (CONV_1_FH * WINDOW_OUT * CONV_1_FW * WINDOW_OUT * CONV_1_OCH) // output size
 // constexpr int CONV_2_OUTPUT_SIZE = CONV_1_FH * WINDOW_OUT_2 * CONV_1_FW * WINDOW_OUT_2 * CONV_1_OCH; // output size
 constexpr int CONV_2_OUTPUT_SIZE = CONV_2_OH * CONV_2_OW * CONV_2_OCH; // output size
 
@@ -77,26 +77,26 @@ using filter_stream_t = hls::stream<ap_int<8>>;
 
 // typedef ap_int<45> memory_in_t[CONV_0_FW * CONV_0_FH * CONV_0_ICH_PAR][(CONV_0_ICH * (WINDOW_IN * CONV_0_FW) * (WINDOW_IN * CONV_0_FH)) / (CONV_0_FW * CONV_0_FH * CONV_0_ICH_PAR)];
 // typedef ap_int<45> memory_out_t[CONV_1_OH][CONV_1_OW][CONV_1_OCH];
-typedef ap_int<64> memory_in_t;
-typedef ap_int<64> memory_out_t;
+typedef ap_uint<8> memory_in_t;
+typedef ap_int<32> memory_out_t;
 
 //--------------------------MEMORY IN AND OUT OF CONVOLUTION LAYER------------------------------
 template<int FW, int FH, int ICH_PAR, int ICH, int WINDOW_IN>
-using memory_in_conv_t = ap_int<45>[FW * FH * ICH_PAR][(ICH * FW * FH * WINDOW_IN * WINDOW_IN) / (FW * FH * ICH_PAR)];
+using memory_in_conv_t = ap_uint<8>[FW * FH * ICH_PAR][(ICH * FW * FH * WINDOW_IN * WINDOW_IN) / (FW * FH * ICH_PAR)];
 
 template<int FW_OUT, int FH_OUT, int ICH_PAR_OUT, int OCH, int WINDOW_OUT>
-using memory_out_conv_t = ap_int<45>[FW_OUT * FH_OUT * ICH_PAR_OUT][(OCH * FW_OUT * FH_OUT * WINDOW_OUT * WINDOW_OUT) / (FW_OUT * FH_OUT * ICH_PAR_OUT)];
+using memory_out_conv_t = ap_int<32>[FW_OUT * FH_OUT * ICH_PAR_OUT][(OCH * FW_OUT * FH_OUT * WINDOW_OUT * WINDOW_OUT) / (FW_OUT * FH_OUT * ICH_PAR_OUT)];
 
 //--------------------------NUMBER OF IMAGES--------------------------------
 // constexpr int NR_IMG = 5;
 
 //--------------------------VARIABLE FOR INPUT STREAM--------------------------------
-using mem_in_t  = ap_int<64>;
+using mem_in_t  = ap_uint<8>;
 // ‑‑ Output data (64‑bit axis + TLAST)
-using mem_out_t = ap_axiu<64,0,0,0>;
+using mem_out_t = ap_axiu<32,0,0,0>;
 
 
 template<int FW, int FH, int ICH_PAR>
-using conv_packet_t = ap_int<FW * FH * ICH_PAR * 64>;
+using conv_packet_t = ap_uint<FW * FH * ICH_PAR * 8>;
 
 #endif // PARAMETER_H
