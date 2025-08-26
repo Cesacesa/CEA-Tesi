cac48b432b0d95a8cb63e90299a8ed023491156f
lorenzorizzo01 <s323185@tudenti.polito.it>
2025-08-26 08:20:51 +0000

    Update template quant

--- operation_on_matrix.h
+++ operation_on_matrix.h
@@ -1,30 +1,33 @@
-#ifndef OPERATOIN_MATRIX
-#define OPERATOIN_MATRIX
-#include "parameters_dq.h"
+#ifndef OPERATION_MATRIX
+#define OPERATION_MATRIX
+
+#include <ap_int.h>
 #include "dequant_and_quant.h"
- 
-inline void matrix_wrapper(ap_uint<BIT_INPUT> input[HEIGHT][WIDTH],ap_uint<BIT_QUANT> output[HEIGHT][WIDTH]){
-    #ifndef __SYNTHESIS__
-    static std::ofstream debug_file("matrix_check.txt", std::ios::app); // apertura in append
+#ifndef __SYNTHESIS__
+#include <fstream>
 #endif
- 
-   
-   
-            Height_loop: for (int h = 0; h < HEIGHT; h++) {
-                Width_loop: for (int w = 0; w < WIDTH; w++) {
-                    #pragma HLS PIPELINE II=1
- 
-                    output[h][w]= deq_and_quant(input[h][w]);
-                    #ifndef __SYNTHESIS__
-                        debug_file << "INPUT:  " << input[h][w] << "\n";
-                        debug_file << "h:  " << h << "\n";
-                        debug_file << "w:  " << w << "\n";
-                        debug_file << "OUTPUT: " << output[h][w] << "\n";
-                        debug_file <<"-----------------------------\n";
-                    #endif
-                }
-            }
+
+template<int HEIGHT, int WIDTH, int BIT_INPUT, int BIT_QUANT, int SHIFT_FACTOR_TOT>
+inline void matrix_wrapper(ap_uint<BIT_INPUT> input[HEIGHT][WIDTH],
+                           ap_uint<BIT_QUANT> output[HEIGHT][WIDTH]) {
+#ifndef __SYNTHESIS__
+    static std::ofstream debug_file("matrix_check.txt", std::ios::app);
+#endif
+
+    Height_loop: for (int h = 0; h < HEIGHT; h++) {
+        Width_loop: for (int w = 0; w < WIDTH; w++) {
+#pragma HLS PIPELINE II=1
+            output[h][w] = deq_and_quant<BIT_INPUT, BIT_QUANT, SHIFT_FACTOR_TOT>(input[h][w]);
+
+#ifndef __SYNTHESIS__
+            debug_file << "INPUT:  " << input[h][w] << "\n";
+            debug_file << "h:  " << h << "\n";
+            debug_file << "w:  " << w << "\n";
+            debug_file << "OUTPUT: " << output[h][w] << "\n";
+            debug_file << "-----------------------------\n";
+#endif
         }
- 
- 
+    }
+}
+
 #endif
